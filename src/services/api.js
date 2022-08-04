753070303ce3e826247bdb5fa674d6c24037e8f6
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-08-04 14:37:33 -0300

    Set https api request

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const API_KEY = '83559090acfc4e3e935142404220408';
 
 const fetchData = async (city) => {
-  const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+  const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
   const fetchResponse = await fetch(API_URL);
   const data = await fetchResponse.json();
 
