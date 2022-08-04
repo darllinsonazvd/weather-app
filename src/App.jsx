65596141c7c4359bc4806572c70b8d690877bde2
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-08-04 01:19:33 -0300

    Install phosphor-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { MagnifyingGlass } from 'phosphor-react';
+
 export default function App() {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <form>
-        <input type="text" placeholder="Cidade" />
-        <button type="submit">Pesquisar</button>
+      <form className="flex justify-center items-center">
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+        />
+        <button
+          type="submit"
+          className="p-3 ml-2 rounded-lg bg-violet-700 text-gray-50"
+        >
+          <MagnifyingGlass size={20} />
+        </button>
       </form>
     </div>
   );
